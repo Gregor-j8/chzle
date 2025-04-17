a6aed04a8975e90b6018d05fd4b5b79d32ff3c1d
gregor-J8 <gregor.johnson@outlook.com>
2025-04-17 08:52:01 -0500

    setup trpc

---
+++ app/server/context.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const createContext = () => {
+  return {
+    prisma,
+  };
+};
+
+export type Context = ReturnType<typeof createContext>;
---
+++ app/server/routers/game.ts
@@ -0,0 +1,21 @@
+// server/routers/game.ts
+import { z } from "zod";
+import { router, publicProcedure } from "../trpc";
+
+export const gameRouter = router({
+  createGame: publicProcedure
+    .input(z.object({
+      whiteId: z.string(),
+      blackId: z.string(),
+    }))
+    .mutation(async ({ input, ctx }) => {
+      return await ctx.prisma.game.create({
+        data: {
+          whiteId: input.whiteId,
+          blackId: input.blackId,
+          moves: '',
+          result: '',
+        },
+      });
+    }),
+});
