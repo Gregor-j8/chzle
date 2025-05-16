5fdcff6caaccf2886f155e2761b96c7f08e9d710
gregor-J8 <gregor.johnson@outlook.com>
2025-05-16 07:24:49 -0500

    fixed websocket

--- utils/trpc.ts
+++ utils/trpc.ts
@@ -1,27 +1,15 @@
-import { createTRPCReact } from '@trpc/react-query';
-import type { AppRouter } from '../server/routers/_app';
-import { createWSClient, wsLink } from '@trpc/client';
 import { httpBatchLink } from '@trpc/client';
-import { splitLink } from '@trpc/client';
+import { createTRPCReact } from "@trpc/react-query";
+import type { AppRouter } from "../server/routers/_app";
 import superjson from 'superjson';
 
 export const trpc = createTRPCReact<AppRouter>();
-const wsClient = createWSClient({
-  url: 'ws://localhost:3001',
-})
 
 export const trpcClient = trpc.createClient({
   links: [
-    splitLink({
-      condition: (op) => op.type === 'subscription',
-      true: wsLink({
-        client: wsClient,
-        transformer: superjson,
-      }),
-      false: httpBatchLink({
+    httpBatchLink({
         url: '/api/trpc',
-        transformer: superjson,
-      }),
+        transformer: superjson
     }),
   ],
 });
