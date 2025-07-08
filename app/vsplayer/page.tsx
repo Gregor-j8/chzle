7cca8711173a4303c90c3b6634d53cd2ab7a3fdf
gregor-J8 <gregor.johnson@outlook.com>
2025-07-07 21:19:52 -0500

    fixed multiplayer game page

--- app/vsplayer/page.tsx
+++ app/vsplayer/page.tsx
@@ -24,7 +24,7 @@ export default function Lobby() {
     if (!user) return router.push('/')
 
     const roomId = roomIdInput.trim()
-    if (!roomId) return
+    if (!roomId) return toast.error("invalid game code", {duration: 3000, position: "top-center", style: { marginTop: "50px" }})
 
     const { data: game } = await supabase
       .from('games')
@@ -35,7 +35,7 @@ export default function Lobby() {
     if (game.white_player && game.black_player
       && user.id !== game.white_player
       && user.id !== game.black_player) {
-      toast.error('Game is full.')
+      toast.error('Game is full.', {duration: 3000, position: "top-center", style: { marginTop: "50px" }})
       return
     }
 
@@ -49,18 +49,19 @@ export default function Lobby() {
   }
 
   return (
-<div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 px-4">
-  <div className="bg-gray-800 rounded-2xl shadow-xl p-8 w-full max-w-sm flex flex-col gap-6">
-    <button onClick={handleCreate} className="bg-blue-600 hover:bg-blue-700 transition text-white px-6 py-3 rounded-lg font-semibold">
-      🎲 Create Game
-    </button>
-    <input type="text"
-      className="border border-gray-600 bg-gray-700 text-white placeholder-gray-400 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-      placeholder="Enter Room ID" value={roomIdInput} onChange={(e) => setRoomIdInput(e.target.value)}/>
-    <button onClick={handleJoin} className="bg-green-600 hover:bg-green-700 transition text-white px-6 py-3 rounded-lg font-semibold">
-      🔗 Join Game
-    </button>
-  </div>
-</div>
+    <div className="overflow-hidden min-h-screen bg-gray-900 px-4 py-10">
+      <h1 className="text-center text-2xl font-bold text-white mb-6">Multiplayer Game</h1>
+      <div className="mx-auto w-11/12 lg:w-4/12 max-w-2xl flex flex-col text-white border border-gray-700 p-6 rounded-xl bg-gray-800 gap-6">
+        <button onClick={handleCreate} className="bg-blue-600 hover:bg-blue-700 transition text-white px-6 py-3 rounded-md font-semibold cursor-pointer">
+          Create Game
+        </button>
+        <input type="text" placeholder="Enter Room ID" value={roomIdInput} onChange={(e) => setRoomIdInput(e.target.value)}
+          className="border border-gray-600 bg-gray-700 text-white placeholder-gray-400 px-4 py-2 rounded-md cursor-pointer focus:ring-blue-500"/>
+        <button onClick={handleJoin} 
+        className="border bg-gray-800 border-gray-500 p-2 px-4 rounded-md text-gray-300 hover:bg-gray-700-md font-semibold cursor-pointer">
+          Join Game
+        </button>
+      </div>
+    </div>
   )
 }
