b66c94a206dd40bcc77d4d5d44c3db9090230630
gregor-J8 <gregor.johnson@outlook.com>
2025-07-12 16:25:35 -0500

    added catch for no matches and fixed bug with @ in pgn

--- app/gamereview/GameList.tsx
+++ app/gamereview/GameList.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { trpc } from "@/utils/trpc"
 import { ArrowLeft, ArrowRight, X } from "lucide-react"
 import { LoadingPage } from "../_components/loading"
@@ -12,10 +13,21 @@ export default function GameReviewModal({ username, onClose }) {
   const [year, setYear] = useState(currentYear)
   const [month, setMonth] = useState(currentMonth)
   const [page, setPage] = useState(1)
-  const { data, isLoading } = trpc.gameReviewRouter.getGameReview.useQuery({ username, year, month, page }, { enabled: !!username })
+  const [gameData, setGameData] = useState<any>(null)
+  const [IsUpdating, setIsUpdating] = useState(false)
+
+  const { data, isLoading, isFetching } = trpc.gameReviewRouter.getGameReview.useQuery({ username, year, month, page }, { enabled: !!username })
+
+  useEffect(() => {
+    if (data) {
+      setGameData(data)
+      setIsUpdating(false)
+    }
+  }, [data])
 
   const handleNext = () => {
     if (!data) return
+    setIsUpdating(true)
     if (page < data.totalPages) {
       setPage(prev => prev + 1)
     } else {
@@ -30,7 +42,7 @@ export default function GameReviewModal({ username, onClose }) {
   const handlePrev = () => {
     const future = (month === 12 && year + 1 > currentYear) || (month < 12 && year === currentYear && month + 1 > currentMonth)
     if (future) return
-
+    setIsUpdating(true)
     if (page > 1) {
       setPage(prev => prev - 1)
     } else {
@@ -43,47 +55,48 @@ export default function GameReviewModal({ username, onClose }) {
     }
   }
 
-  if (isLoading) return <LoadingPage />
-console.log(data)
+  if (isLoading && !gameData) return <LoadingPage />
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-60">
       <div className="bg-gray-900 text-white w-full max-w-2xl p-6 rounded-xl shadow-lg border border-gray-700 overflow-hidden">
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-xl font-semibold">Games for {month}/{year}</h2>
-          <button onClick={onClose} className="text-gray-400 hover:text-white"><X/></button>
+          <button onClick={onClose} className="text-gray-400 hover:text-white"><X /></button>
         </div>
 
-        {data && (
+        {gameData && (
           <div>
-            <p className="text-sm text-gray-400 mb-4">Page {page} of {data.totalPages}</p>
-
-            <div className="max-h-96 overflow-auto text-sm border border-gray-700 bg-gray-800 rounded-md p-4 space-y-3">
-              {data.games.map((game, i) => (
-               <div key={i} className="p-2 bg-gray-700 rounded-md border border-gray-600 hover:bg-gray-600 flex flex-col gap-3">
-                <div className="flex justify-between items-center gap-4 flex-wrap">
-                  <div className="flex flex-col md:flex-row md:items-center md:gap-6 text-gray-100">
-                    <div className="space-y-1">
-                      <p>White: {game?.tags.White}</p>
-                      <p>Black: {game?.tags.Black}</p>
+            {gameData.games.length > 0 ? (
+              <div>
+                <p className="text-sm text-gray-400 mb-4">Page {page} of {gameData.totalPages}</p>
+                <div className="max-h-96 overflow-auto text-sm border border-gray-700 bg-gray-800 rounded-md p-4 space-y-3">
+                  {gameData.games.map((game, i) => (
+                    <div key={i} className="p-2 bg-gray-700 rounded-md border border-gray-600 hover:bg-gray-600 transition flex flex-col gap-3">
+                      <div className="flex justify-between items-center gap-4 flex-wrap">
+                        <div className="flex flex-col md:flex-row md:items-center md:gap-6 text-gray-100">
+                          <div className="space-y-1">
+                            <p>White: {game?.tags.White}</p>
+                            <p>Black: {game?.tags.Black}</p>
+                          </div>
+                          <p className="text-center text-gray-300 font-bold">
+                            Result: {game?.tags.Result === "1-0" ? `${game?.tags.White} wins` : game?.tags.Result === "0-1" ? `${game?.tags.Black} wins` : "Draw"}
+                          </p>
+                        </div>
+                        <div className="ml-auto">
+                          <Chessboard boardWidth={60} position={game?.tags.CurrentPosition} />
+                        </div>
+                      </div>
                     </div>
-                    <p className="text-center text-gray-300 font-bold">
-                      Result: {game?.tags.Result === "1-0" ? `${game?.tags.White} wins` : game?.tags.Result === "0-1" ? `${game?.tags.Black} wins` : "Draw"}
-                    </p>
-                  </div>
-
-                  <div className="ml-auto">
-                    <Chessboard boardWidth={60} position={game?.tags.CurrentPosition} />
-                  </div>
+                  ))}
                 </div>
               </div>
-              ))}
-            </div>
+            ) : 'Not found please try a different month or different user'}
 
             <div className="flex justify-between items-center mt-4">
-              <button onClick={handleNext} className="px-4 py-1 bg-gray-700 text-white rounded hover:bg-gray-600 disabled:opacity-50">
+              <button onClick={handleNext} disabled={isFetching || IsUpdating} className="px-4 py-1 bg-gray-700 text-white rounded hover:bg-gray-600 disabled:opacity-50">
                 <ArrowLeft />
               </button>
-              <button onClick={handlePrev} disabled={(month === currentMonth && year === currentYear && page === 1)}
+              <button onClick={handlePrev} disabled={(month === currentMonth && year === currentYear && page === 1) || isFetching || IsUpdating}
                 className="px-4 py-1 bg-gray-700 text-white rounded hover:bg-gray-600 disabled:opacity-50">
                 <ArrowRight />
               </button>
